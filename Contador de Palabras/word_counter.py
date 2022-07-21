cc50963bf77c0a26b1ae299fba3443663e291913
alejandrolederman <alejandro.lederman@live.com.ar>
2022-07-21 19:03:20 -0300

    actualizacion palabras

--- Contador de Palabras/word_counter.py
+++ Contador de Palabras/word_counter.py
@@ -1,17 +1,34 @@
 import os
-notas = open ("./Contador de Palabras/phrases.txt")
+here = os.path.dirname(os.path.abspath(__file__))
+notas = os.path.join(here, 'phrases.txt')
+archivo=open(notas,"w")
 
 frase =input('ingrse una frase: ')
+archivo.writelines([frase, " "])
+archivo.close()
 
-if frase == "abra cadabra" or frase =="show time" or frase =="the end":
-    print (" fin del programa")
+frases = 0
+palabras = 0
+total = 0
+
+
+while  frase != "abra cadabra" and frase !="show time" and frase !="the end":
+    
+    frases = frases + 1
+    palabras = len(frase.split(' ')) 
+    total = total + palabras   
+    
+    print(frase.split(' '))
+    print("en total hay " + str(palabras) + " palabras.")
+    print ("cantidad de palabras que se ingresaron:    ", frases)
+    print ("total de todas las palabras que se guardaron:   ", total )
+    
+    frase =input('ingrse una frase: ')
+    archivo=open(notas,"a")
+    archivo.writelines([frase," "])
+    archivo.close()
     
 else:
     
-    # for i in range (len(frase.split(' ')) ):
-    #     notas.write[i]
-        
-    result = len(frase.split(' '))    
-    print(frase.split(' '))
-
-    print("en total hay " + str(result) + " palabras.")
+    print (" fin del programa")
+  
