5f02033e4e804b4fec795e49e4a7aa9d6282fac9
marlevit <109757267+marlevit@users.noreply.github.com>
2022-07-25 03:02:38 -0300

    Add files via upload

---
+++ Fibonacci/fibonacci.py
@@ -0,0 +1,16 @@
+z=0
+while z<=0:
+    z=int(input("Ingrese cant.de números de Fibonacci <mayor que 0> : "))
+
+else:
+  a=0
+  b=1
+  c=1
+  for i in range (0,z):
+      print(c)
+      c=a+b
+      a=b
+      b=c
+       
+# Lista de ejemplo de la sucesion de Fibonacci 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377…
+# para verificar resultados.    
