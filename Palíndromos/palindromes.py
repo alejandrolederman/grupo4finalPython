f029bfb18e31eaf5366bbecc2292d0ff12cbbc5e
marlevit <109757267+marlevit@users.noreply.github.com>
2022-07-24 21:57:18 -0300

    Add files via upload

--- Palíndromos/palindromes.py
+++ Palíndromos/palindromes.py
@@ -1,30 +1,21 @@
-# - El programa esta incompleto, el usuario deberia seguir ingresando palabras hasta que ingrese una palabra clave.
-#   Ademas, deberia ir mostrando en consola la cantidad de palabras palindromo y no palindromo ingrasadas en cada iteracion
+lista_si_palin=[]
+lista_no_palin=[]
+a="str aleatoria"
 
-palabra = str(input("ingrese palabra: "))
-sonPalindromos = 0
-noPalin = 0
-
-while palabra != "aloha" and palabra != "bye":
-    
-    largoDeLaPalabra= len(palabra)
-    palabraInvertida=str("")
-    i =int(largoDeLaPalabra)
-    
-    while (i>0):
-        i=i-1
-        palabraInvertida=palabraInvertida+palabra[i]
-        
-    if(palabra==palabraInvertida):
-        sonPalindromos = sonPalindromos + 1
-        print("es palindromo")
-        print ("palindromes totales:", sonPalindromos)
-        print ("total de no palindromes: ", noPalin)
-        palabra = str(input("ingrese palabra: "))
+while a!="salir":
+    a=input("Ingrese la palabra o <salir> para salir: ")
+    if a=="".join(reversed(a)):
+         print("Es Palindrome")
+         lista_si_palin.append(a)
         
     else:
-        print("no es un palindromo ")
-        noPalin = noPalin + 1
-        print ("palindromes totales:", sonPalindromos)
-        print ("total de no palindromes: ", noPalin)
-        palabra = str(input("ingrese palabra: "))
+            if a=="salir":
+             print(f"Usted ingresó {len(lista_si_palin)} palabras palindromes y éste es el listado: {lista_si_palin}")
+             print(f"Usted ingresó {len(lista_no_palin)} palabras NO palindromes y éste es el listado: {lista_no_palin}")
+        
+            else:
+             print("No es Palindrome")
+             lista_no_palin.append(a)
+
+       
+    
