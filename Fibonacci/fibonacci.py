562a22bb9c224761332b7ef91362d62efc08f65b
marlevit <109757267+marlevit@users.noreply.github.com>
2022-07-25 21:53:24 -0300

    Add files via upload
    Versión que advierte añ asuario si el número es negativo o cero.
    También verifica si el ingreso es decimal, en ese caso lo advierte al usuario, lo convierte a entero y ejecuta.

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -1,21 +1,24 @@
 z=0
-
 while z<=0:
-    
-    z=int(input("Ingrese cant.de números de Fibonacci <mayor que 0> : "))
-
+    z=float(input("Ingrese cant.de números de Fibonacci deseada <mayor que 0> : "))
+    if z!=int(z):
+        print("*** Un número decimal será considerado como un número entero ***")
+    z=int(z)
+    if z==0:
+        print("¡Dije números mayores que cero!")
+        print("")
+    if z<0:
+        print("¡Dije sólo números positivos!")
+        print("")  
 else:
-    
-  a=0
-  b=1
-  c=1
-  
-  for i in range (0,z):
-      
-      print(c)
-      c=a+b
-      a=b
-      b=c
+     a=0
+     b=1
+     c=1
+     for i in range (0,z):
+        print(c)
+        c=a+b
+        a=b
+        b=c
        
 # Lista de ejemplo de la sucesion de Fibonacci 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377…
 # para verificar resultados.    
