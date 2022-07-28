dd6a700d002a65150a2f79b1ed5b4331cfa750a3
marlevit <109757267+marlevit@users.noreply.github.com>
2022-07-28 00:39:40 -0300

    Add files via upload
    Correcciones hechas: Debe imprimir tambien el total de palabras ingresadas (cantidad de palindromos + cantidad de no palindromos).

--- Palíndromos/palindromes.py
+++ Palíndromos/palindromes.py
@@ -6,16 +6,19 @@ while a!="salir":
     a=input("Ingrese la palabra o <salir> para salir: ")
     if a=="".join(reversed(a)):
          print("Es Palindrome")
+         print("")
          lista_si_palin.append(a)
         
     else:
             if a=="salir":
-             # Falta imprimir el total de palabras ingresadas (cantidad de palindromos + cantidad de no palindromos)
              print(f"Usted ingresó {len(lista_si_palin)} palabras palindromes y éste es el listado: {lista_si_palin}")
+             print("")
              print(f"Usted ingresó {len(lista_no_palin)} palabras NO palindromes y éste es el listado: {lista_no_palin}")
-        
+             print("")
+             print(f"El total de palabras ingresadas es de: {len(lista_no_palin)+len(lista_si_palin)}")
             else:
              print("No es Palindrome")
+             print("")
              lista_no_palin.append(a)
 
        
