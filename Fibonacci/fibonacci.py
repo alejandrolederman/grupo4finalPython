99d62f99eb08304dd1665a68c14f2a7a6b1c20ef
marlevit <109757267+marlevit@users.noreply.github.com>
2022-07-29 21:29:05 -0300

    Add files via upload
    En esta versión agregué un TRY / EXCEPT, para evitar errores de ingreso de decimales o letras
    y también agregué un  ( end=" " ) para que el resultado lo imprima de manera horizontal y con espaciado.

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -1,24 +1,31 @@
 z=0
 while z<=0:
-    z=float(input("Ingrese cant.de números de Fibonacci deseada <mayor que 0> : "))
-    if z!=int(z):
-        print("*** Un número decimal será considerado como un número entero ***")
-    z=int(z)
-    if z==0:
-        print("¡Dije números mayores que cero!")
+    try:
+        z=int(input("Ingrese cant.de números de Fibonacci deseada <mayor que 0> : "))
+    
+        if z==0:
+            print("¡Dije números mayores que cero!")
+            print("")
+        if z<0:
+            print("¡Dije sólo números positivos!")
+            print("")  
+    
+            
+    except:
         print("")
-    if z<0:
-        print("¡Dije sólo números positivos!")
-        print("")  
+        print("No es posible ingresar letras ni números con decimales")    
+        print("")
+        
+        
 else:
      a=0
      b=1
      c=1
      for i in range (0,z):
-        print(c)
+        print(c,end="  ")
         c=a+b
         a=b
         b=c
-       
+     
 # Lista de ejemplo de la sucesion de Fibonacci 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377…
 # para verificar resultados.    
